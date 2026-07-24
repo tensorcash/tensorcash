4b74123fa151f72f2d0eeff453b4f772fac27ef5
Dizzztroyer <dizzztroyer@users.noreply.github.com>
2026-07-24 12:51:33 +0300

    packaging: remove old windows scripts (replaced by windows-wallet-build/)

--- packaging/windows/go.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-del tensorcash-data\tensor\.lock tensorcash-data\tensor\bitcoind.pid 2>nul
-taskkill /f /im tensorcash-qt.exe 2>nul
-start /B "" tensorcash-qt.exe -datadir="%cd%\tensorcash-data"
-set /p WIF=Enter WIF private key: 
-py tensorcash_import_key.py --wif "%WIF%" --wallet "main" --backup "%cd%\tensorcash-recovered-wallet.dat" --timestamp 0 --explicit-rescan
-pause
